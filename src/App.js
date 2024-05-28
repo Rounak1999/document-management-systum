9fdde3103ba7f3320c01a5bd6bb3512195d0356d
Rounak Gour <rounakgour110@gmail.com>
2024-05-28 09:21:20 +0530

    creating structure and initialize components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Home from "./pages/main/Home";
+import Login from "./pages/auth/Login";
+// import ErrPage from "./components/ErrPage";
+import './App.css'
 
 function App() {
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    setToken(token);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    !token ? <Login /> : <Home />
   );
 }
 
