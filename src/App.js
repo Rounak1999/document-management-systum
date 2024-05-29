f6839cc69fa96d33cf7818bd5de1df6e39869a72
Rounak Gour <rounakgour110@gmail.com>
2024-05-29 18:59:19 +0530

    main page and next routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/main/Home";
 import Login from "./pages/auth/Login";
 import ErrPage from "./components/ErrPage";
 import './App.css'
+import UploadDocument from "./pages/main/UploadDocument";
+import SearchDocument from "./pages/main/SearchDocument";
 
 function App() {
 
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     let token = localStorage.getItem("token");
@@ -16,12 +18,15 @@ function App() {
 
   return (
     <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="*" element={<ErrPage />}
-        />
+        <Route path="/upload_document" element={<UploadDocument />} />
+        <Route path="/search_document" element={<SearchDocument />} />
+        <Route path="*" element={token === "" && token === undefined && token === null ? <Navigate to="/" replace={true} /> : <Navigate to="/home" replace={true} />} />
       </Routes>
+
     </BrowserRouter>
     // !token ? <Login /> : <Home />
   );
--- src/apis.js
+++ src/apis.js
@@ -3,14 +3,12 @@ import {environment} from './environment';
 export const generateOTP = async (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            // let token = await Utils.getData('token');
             let fetchParameter = {
                 method: 'POST',
                 body: JSON.stringify(data),
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                    // 'Authorization': 'Bearer ' + token
                 },
             }
             let serverResponse = await fetch(environment.apiUrl + 'generateOTP', fetchParameter);
@@ -27,14 +25,12 @@ export const generateOTP = async (data) => {
 export const validateOTP = async (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            // let token = await Utils.getData('token');
             let fetchParameter = {
                 method: 'POST',
                 body: JSON.stringify(data),
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                    // 'Authorization': 'Bearer ' + token
                 },
             }
             let serverResponse = await fetch(environment.apiUrl + 'validateOTP', fetchParameter);
@@ -54,7 +50,7 @@ export const saveDocumentEntry = async (data) => {
             let token = await localStorage.getItem('token');
             let fetchParameter = {
                 method: 'POST',
-                body: JSON.stringify(data),
+                body: data,
                 headers: {
                     'token': JSON.parse(token)
                 },
